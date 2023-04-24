727dbe4dbe4f2ea0c559844d12e2196f14e56070
Nicolas Ayan <nicolas.ayan18'@'gmail.com>
2023-04-24 13:50:29 -0300

    Added it a delete button

--- index.js
+++ index.js
@@ -2,57 +2,71 @@ function getId(id) {
   return document.getElementById(id)
 }
 
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const openModalBtn = document.querySelector(".btn-open");
-const closeModalBtn = document.querySelector(".btn-close");
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const openModalBtn = document.querySelector('.btn-open')
+const closeModalBtn = document.querySelector('.btn-close')
 
 const openModal = function () {
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-};
+  modal.classList.remove('hidden')
+  overlay.classList.remove('hidden')
+}
 
 const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-};
+  modal.classList.add('hidden')
+  overlay.classList.add('hidden')
+}
 
-closeModalBtn.addEventListener("click", closeModal);
-openModalBtn.addEventListener("click", openModal);
+openModalBtn.addEventListener('click', openModal)
+closeModalBtn.addEventListener('click', closeModal)
 
+//const form  = getId('form');
+const container = getId('container')
+container.addEventListener('click', deleteToDo)
+//text = getId("text").value
 
-const form  = getId('form');
-row = getId("container")
-text = getId("text").value
-
-form.addEventListener('submit', (event) => {
-  const text = getId("text").value
+form.addEventListener('submit', event => {
+  event.preventDefault()
+  const text = getId('text').value
   console.log(text)
   painToDo(text)
-  text.value = ""
+  text.value = ''
+  getId('text').value = ''
   closeModal()
-  event.preventDefault()
-});
+})
 
 function createIdRandom() {
-  randomId = Math.floor(Math.random() * 1000)
-  return randomId
+  return Date.now()
 }
 
 function painToDo(text) {
   randomId = createIdRandom()
-  let contentToDo = document.createElement('div') 
-  contentToDo.setAttribute("class", "to-do")
-   contentToDo.innerHTML = `
+ /*  console.log(randomId) */
+  let contentToDo = document.createElement('div')
+  contentToDo.setAttribute('class', 'to-do')
+  contentToDo.innerHTML = `
   <input class="to-do__check" type="checkbox">
-  <p class="to-do__text">${text}</p>
-  <button class="to-do__delete modal-btn" id="${randomId}" onclick=deleteToDo(id)>delete</button>
+  <p class="to-do__text checked">${text}</p>
+  <button class="to-do__delete modal-btn" id="${randomId}" data-id="${randomId}" >delete</button>
   </tr>`
-  row.appendChild(contentToDo)
+  container.appendChild(contentToDo)
 }
 
-function deleteToDo(id) {
-  const id = getId(id)
-  return id.parentElement.remove()
-}
+/* const deleteBtn = document.querySelector('.to-do__delete')
+deleteBtn.addEventListener('click', deleteToDo) */
+/* const deleteBtn = document.querySelectorAll('.to-do__delete')
+deleteBtn.forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    console.log(e);
+  })
+})
+ */
 
+
+function deleteToDo(e) {
+  console.log(e.target.dataset.id);
+  const id = e.target.dataset.id
+  if (id) {
+    e.target.parentElement.remove()
+  }
+}
